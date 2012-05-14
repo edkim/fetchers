df78315aac5225beba1c7bce6c831937f29bf710
Edward Kim <edk217@gmail.com>
2012-05-14 19:36:06 -0400

    get incidents

--- spec/traffic_fetcher_spec.rb
+++ spec/traffic_fetcher_spec.rb
@@ -2,10 +2,20 @@ require './../traffic_fetcher.rb'  # more elegant way to require?
 
 module Traffic
 	describe MapQuestFetcher do
-		it "returns data" do
+		before (:each) do
 			@fetcher = MapQuestFetcher.new
-			@fetcher.run
-			@fetcher.data.should_not be_nil
 		end
+
+
+		# it "gets traffic data" do		
+		# 	@fetcher.run
+		# 	@fetcher.data.should_not be_nil
+		# end
+
+		it "returns traffic incident data" do
+			@data = @fetcher.get_incidents
+			@data[1].should_not be_nil
+		end
+
 	end
 end
