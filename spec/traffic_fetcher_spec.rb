5f002a13f3e959740ce2207ebd738d5902f8b8e5
Edward Kim <edk217@gmail.com>
2012-05-16 10:26:53 -0400

    convert tabs to spaces

--- spec/traffic_fetcher_spec.rb
+++ spec/traffic_fetcher_spec.rb
@@ -1,28 +1,28 @@
 require '../traffic_fetcher.rb'  # more elegant way to require?
 
 module Traffic
-	describe MapQuestFetcher do
-		before (:all) do
-			@fetcher = MapQuestFetcher.new('Fmjtd%7Cluua2d6t2u%2Cb5%3Do5-hr85g')
-		end
+  describe MapQuestFetcher do
+    before (:all) do
+      @fetcher = MapQuestFetcher.new('Fmjtd%7Cluua2d6t2u%2Cb5%3Do5-hr85g')
+    end
 
-		describe '#fetch' do
-			it "fetches traffic data for zip code" do
-				@fetcher.fetch("02806").should_not be_empty
-			end
-			
-			it "fetches traffic data for city, state" do
-				@fetcher.fetch("Baltimore, MD").should_not be_empty
-			end
+    describe '#fetch' do
+      it "fetches traffic data for zip code" do
+        @fetcher.fetch("02806").should_not be_empty
+      end
+      
+      it "fetches traffic data for city, state" do
+        @fetcher.fetch("Baltimore, MD").should_not be_empty
+      end
 
-		end
-	end
+    end
+  end
 
-	describe BingFetcher do
-		it "returns traffic incident data from Bing" do
-			@fetcher = BingFetcher.new('AmAZzRl0lPmqYJ3i_GW9oaItcaSHgQnphkE1xl6N-1P5gyg68KPWVqvmlJwnxcCN')
-			@data = @fetcher.get_incidents
-			@data.should_not be_empty
-		end
-	end
+  describe BingFetcher do
+    it "returns traffic incident data from Bing" do
+      @fetcher = BingFetcher.new('AmAZzRl0lPmqYJ3i_GW9oaItcaSHgQnphkE1xl6N-1P5gyg68KPWVqvmlJwnxcCN')
+      @data = @fetcher.get_incidents
+      @data.should_not be_empty
+    end
+  end
 end
--- traffic_fetcher.rb
+++ traffic_fetcher.rb
@@ -2,79 +2,77 @@ require "net/http"
 require "json"
 
 module Traffic
+  DEFAULT_RADIUS = 0.181 #Used to generate bounding box from single lat/long
 
-	DEFAULT_RADIUS = 0.181 #Used to generate bounding box from single lat/long
+  class MapQuestFetcher
+    attr_reader :data
+        
+    MQ_TRAFFIC_OPTIONS = "&filters=incidents"
 
+    def initialize (api_key)
+      @traffic_url_base = "http://www.mapquestapi.com/traffic/v1/incidents?key=#{api_key}&inFormat=kvp"
+      @geocode_url_base = "http://www.mapquestapi.com/geocoding/v1/address?key=#{api_key}&inFormat=kvp"
+    end
 
-	class MapQuestFetcher
-		attr_reader :data
-				
-		MQ_TRAFFIC_OPTIONS = "&filters=incidents"
+    def fetch (location)
+      location.gsub!(' ', '%20')
+      uri = URI.parse("#{@geocode_url_base}&location=#{location}")
+      response = Net::HTTP.get_response(uri).body
+      coordinates = parse_coordinates_from response     
+      get_incidents_for coordinates, DEFAULT_RADIUS
+    end
 
-		def initialize (api_key)
-			@traffic_url_base = "http://www.mapquestapi.com/traffic/v1/incidents?key=#{api_key}&inFormat=kvp"
-			@geocode_url_base = "http://www.mapquestapi.com/geocoding/v1/address?key=#{api_key}&inFormat=kvp"
-		end
+    def parse_coordinates_from (response)
+      JSON.parse(response)["results"].first["locations"].first["latLng"]      
+    end
 
-		def fetch (location)
-			location.gsub!(' ', '%20')
-			uri = URI.parse("#{@geocode_url_base}&location=#{location}")
-			response = Net::HTTP.get_response(uri).body
-			coordinates = parse_coordinates_from response			
-			get_incidents_for coordinates, DEFAULT_RADIUS
-		end
+    
+    def get_incidents_for (coordinates, radius)
+      lat_ul = coordinates["lat"] + radius
+      long_ul = coordinates["lng"] - radius
+      lat_lr = coordinates["lat"] - radius
+      long_lr = coordinates["lng"] + radius
+      bounding_box = [lat_ul, long_ul, lat_lr, long_lr].join(',')
+      parse_traffic_data_for bounding_box
+    end   
 
-		def parse_coordinates_from (response)
-			JSON.parse(response)["results"].first["locations"].first["latLng"]			
-		end
+    def parse_traffic_data_for (bounding_box)
+      uri = URI.parse("#{@traffic_url_base}&boundingBox=#{bounding_box}#{MQ_TRAFFIC_OPTIONS}")
+      response = Net::HTTP.get_response(uri).body
+      incidents = JSON.parse(response)["incidents"]
 
-		
-		def get_incidents_for (coordinates, radius)
-		  lat_ul = coordinates["lat"] + radius
-		  long_ul = coordinates["lng"] - radius
-			lat_lr = coordinates["lat"] - radius
-		  long_lr = coordinates["lng"] + radius
-		  bounding_box = [lat_ul, long_ul, lat_lr, long_lr].join(',')
-		  parse_traffic_data_for bounding_box
-		end		
-
-		def parse_traffic_data_for (bounding_box)
-			uri = URI.parse("#{@traffic_url_base}&boundingBox=#{bounding_box}#{MQ_TRAFFIC_OPTIONS}")
-		  response = Net::HTTP.get_response(uri).body
-			incidents = JSON.parse(response)["incidents"]
-
-			@data = {
-		  	incident_count: incidents.size,
-		  	incidents: incidents
-		  }
-		end
-	end
+      @data = {
+        incident_count: incidents.size,
+        incidents: incidents
+      }
+    end
+  end
 
 
-	class BingFetcher
-		attr_reader :data, :response				
-		BASE_URL = "http://dev.virtualearth.net/REST/V1/Traffic/Incidents"
+  class BingFetcher
+    attr_reader :data, :response        
+    BASE_URL = "http://dev.virtualearth.net/REST/V1/Traffic/Incidents"
 
 
-		LAT1 = "39.077998" # lower left latitude
-		LONG1 = "-76.887259" # upper left longitude
-		LAT2 = "39.503136" # upper right latitude
-		LONG2 = "-76.337942" # lower right longitude
-		BOUNDING_BOX = [LAT1,LONG1,LAT2,LONG2].join(',')
+    LAT1 = "39.077998" # lower left latitude
+    LONG1 = "-76.887259" # upper left longitude
+    LAT2 = "39.503136" # upper right latitude
+    LONG2 = "-76.337942" # lower right longitude
+    BOUNDING_BOX = [LAT1,LONG1,LAT2,LONG2].join(',')
 
-		def initialize (api_key)						
-			@uri = URI.parse("#{BASE_URL}/#{BOUNDING_BOX}?key=#{api_key}")
-			@response = Net::HTTP.get_response(@uri)
-		end
+    def initialize (api_key)            
+      @uri = URI.parse("#{BASE_URL}/#{BOUNDING_BOX}?key=#{api_key}")
+      @response = Net::HTTP.get_response(@uri)
+    end
 
-		def get_incidents			
-		  incidents = JSON.parse(@response.body)["resourceSets"].first["resources"]		  			 
-		  
-		  @data = {
-   			incident_count: incidents.size,
-   			incidents: incidents
-  		}  		
-		end		
-	end	
+    def get_incidents     
+      incidents = JSON.parse(@response.body)["resourceSets"].first["resources"]            
+      
+      @data = {
+        incident_count: incidents.size,
+        incidents: incidents
+      }     
+    end   
+  end 
 # fetcher = Traffic::BingFetcher.new('AmAZzRl0lPmqYJ3i_GW9oaItcaSHgQnphkE1xl6N-1P5gyg68KPWVqvmlJwnxcCN').get_incidents
 end
